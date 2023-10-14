7edc2fcc5bd274e5588e76caa67d72ba31ab8fa9
unknown <yacobabr@gmail.com>
2023-10-14 16:26:51 -0500

    updated time

--- asset/script.js
+++ asset/script.js
@@ -1,36 +1,50 @@
 var startBtn = document.querySelector(".start-btn");
 // = document.querySelector(".time-counter");
-var questionElement = document.querySelector(".question");
+var questionEl = document.querySelector(".question");
 var choicesContainer= document.querySelector(".choicesContainer");
-var submit= document.querySelector(".submit");
+var submitEl= document.querySelector(".submit");
 
+// startBtn done
+// questionEl
+// submitEl
 var currentQuestionIndex = 0;
+
+
 // string
 var correctAnswer = ["26","25","23"]
 var allQuestions = [
     {
         question: "___is the Programing Launguage for the Web?",
         choice: ['JavaScript','English','Arabic','Chinese'],
-        correctAnswer: 'A'
+        correctAnswer: 'A',
     },
     {
-        question: "?",
-        choice: ['A','B','c','d'],
-        correctAnswer: 'C'
+        question: "what is string?",
+        choice: ['stores a series of characters','dont have any use','take a guess','i do not know'],
+        correctAnswer: 'A',
 
     },
     {
-        question: "what is the ...?",
-        choice: ['A','B','C','D'],
-        correctAnswer: 'B'
+        question: "what is the variable in javaScript?",
+        choice: ['container for storing data','nothing','number','i dont know'],
+        correctAnswer: 'A',
     },
     {
-        question: "....",
-        choice: ['A','B','C','D'],
-        correctAnswer: 'B'
+        question: "what is javaScript",
+        choice: ['programing language','coffe shot','nothing','i dont know'],
+        correctAnswer: 'A',
+    },
+    {  
+        question: "what is addEventlisten is javaScript?",
+        choice: ["its  event holder","kinda like a radio", "nothing", "i dont know"],
+        correctAnswer: "A",
+
     }
     
 ];
+/// add inisial 
+
+
 
 /// time counter after start button
 var timeInterval;
@@ -45,9 +59,7 @@ function timeCounter(){
         if (timeSec <= 0) {
             clearInterval(timeInterval);
         }
-    },1000)
-
-   
+    },1000) 
 
 }
 
@@ -70,7 +82,7 @@ function renderQuestion(){
     var correctAnswer = currentQuestion.correctAnswer
     var choiceBtns = choicesContainer.querySelectorAll('button');
 
-    questionElement.textContent = questionText
+    questionEl.textContent = questionText
     for (var i=0; i< choiceBtns.length; i++) {
         var btn = choiceBtns[i]
         var choice = choiceValues[i];
@@ -93,18 +105,17 @@ renderQuestion();
 /// the time start button must be run once(once: True)
 
 choicesContainer.addEventListener("click", function(){
-    console.log("chioce is on")
+    currentQuestionIndex++
+    renderQuestion();
     // find the nearest button and see if it is right or wrong
 },)
 /// event listner for submitBtn 
 /// the time start button must be run once(once: True)
 
 
-submit.addEventListener("click", function(){
-    console.log("sumit is on")
-    currentQuestionIndex++
-    renderQuestion();
-},)
+submitEl.addEventListener("click", function(){
+    var addInitial= prompt("insert your initial?")
+},{once: true})
 
 
 
